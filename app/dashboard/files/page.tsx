8bf93405649ffa21753ebc9af625a35d8eb77479
slayernominee <slayernominee_git@proton.me>
2024-01-03 16:05:00 +0100

    base project

---
+++ app/dashboard/files/page.tsx
@@ -0,0 +1,10 @@
+import { Sidebar } from "@/components/hyper/sidebar"
+
+export default function Home() {
+    return (
+        <main className="grid grid-cols-6">
+        <Sidebar />
+        <h1>Files</h1>
+        </main>
+    )
+    }
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Sidebar } from '@/components/hyper/sidebar'
+
+export default function Home() {
+    return (
+        <main className="grid grid-cols-6">
+        <Sidebar />
+        <h1>Hello World!</h1>
+        </main>
+        )
+    }
