b1586eab1f56be2c9d622bd12cc886a866b59168
slayernominee <slayernominee_git@proton.me>
2024-01-06 14:07:19 +0100

    listing the index folder works

--- app/dashboard/files/page.tsx
+++ app/dashboard/files/page.tsx
@@ -1,33 +1,70 @@
 "use client"
 import { Layout } from "@/app/components/dash_lay"
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
+  
+
+
+
 
 export default function Home() {
-    const endpoint = process.env.NEXT_PUBLIC_API_URL + "/api/files/"
+
+    const [data, setData] = useState(null);
 
     useEffect(() => {
-        let token = localStorage.getItem("token")
-        if (token === null) {
-            window.location.href = "/login"
+        async function fetchData() {
+            await fetch(process.env.NEXT_PUBLIC_API_URL + '/api/files/&.', {
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('token'),
+                }
+            }).then(async (response) => {
+                if (response.status !== 200) {
+                    console.log('Looks like there was a problem. Status Code: ' + response.status);
+                    return;
+                }
+                setData(await response.json())
+            })
         }
-
-        fetch(endpoint + "&.", {
-            headers: {
-                'Authorization': 'Bearer ' + token
-            },
-            method: 'POST',
-        }).then((resp) => {
-            if (resp.status === 401) {
-                window.location.href = "/login"
-            } else if (resp.status === 200) {
-                console.log(resp)
-            }
-        })
+        fetchData()
     }, [])
 
     return (
             <Layout>
             <h1>Files</h1>
+
+            <Table>
+            <TableCaption>Server Files.</TableCaption>
+            <TableHeader>
+                <TableRow>
+                <TableHead className="w-[100px]">Invoice</TableHead>
+                <TableHead>Name</TableHead>
+                <TableHead>Method</TableHead>
+                <TableHead className="text-right">Modified</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+
+                { data && data.map((item, index) => (
+                    <TableRow key={index}>
+                    <TableCell>x</TableCell>
+                    <TableCell className="font-medium">{item.name}</TableCell>
+                    <TableCell>{`${item.is_dir}x`}</TableCell>
+                    <TableCell className="text-right">-</TableCell>
+                    </TableRow>
+                ))}
+
+            </TableBody>
+            </Table>
+
 
             </Layout>
         )
